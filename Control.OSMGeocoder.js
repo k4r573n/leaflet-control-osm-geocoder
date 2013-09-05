6ed3ee7eb04264ce3c89d175713107ced7864575
Karsten Hinz <k.hinz@tu-bs.de>
2013-09-05 09:41:17 +0200

    local parsing of search message to directly jump to coordinates

--- Control.OSMGeocoder.js
+++ Control.OSMGeocoder.js
@@ -11,7 +11,10 @@ L.Control.OSMGeocoder = L.Control.extend({
 		bounds: null, // L.LatLngBounds
 		email: null, // String
 		callback: function (results) {
-			console.log(results);
+            if (results.length == 0) {
+                console.log("ERROR: didn't find a result");
+                return;
+            }
 			var bbox = results[0].boundingbox,
 			first = new L.LatLng(bbox[0], bbox[2]),
 			second = new L.LatLng(bbox[1], bbox[3]),
@@ -28,6 +31,7 @@ L.Control.OSMGeocoder = L.Control.extend({
 
 	onAdd: function (map) {
 		this._map = map;
+
 		var className = 'leaflet-control-geocoder',
 		container = this._container = L.DomUtil.create('div', className);
 
@@ -66,9 +70,74 @@ L.Control.OSMGeocoder = L.Control.extend({
 
 		return container;
 	},
+    
+    /* helper functions for cordinate extraction */
+    _createSearchResult : function(lat, lon) {
+        //creates an position description similar to the result of a Nominatim search
+        var diff = 0.005;
+        var result = [];
+        result[0] = {};
+        result[0]["boundingbox"] = [parseFloat(lat)-diff,parseFloat(lat)+diff,parseFloat(lon)-diff,parseFloat(lon)+diff];
+        result[0]["class"]="boundary";
+        result[0]["display_name"]="Position: "+lat+" "+lon;
+        result[0]["lat"] = lat;
+        result[0]["lon"] = lon;
+        return result;
+    },
+    _isLatLon : function (q) {
+        //"lon lat" => xx.xxx x.xxxxx
+        var re = /(-?\d+\.\d+)\s(-?\d+\.\d+)/;
+        var m = re.exec(q);
+        if (m != undefined) return m;
+
+        //lat...xx.xxx...lon...x.xxxxx
+        re = /lat\D*(-?\d+\.\d+)\D*lon\D*(-?\d+\.\d+)/;
+        m = re.exec(q);
+        //showRegExpResult(m);
+        if (m != undefined) return m;
+        else return null;
+    },
+    _isLatLon_decMin : function (q) {
+        console.log("is LatLon?: "+q);
+        //N 53° 13.785' E 010° 23.887'
+        //re = /[NS]\s*(\d+)\D*(\d+\.\d+).?\s*[EW]\s*(\d+)\D*(\d+\.\d+)\D*/;
+        re = /([ns])\s*(\d+)\D*(\d+\.\d+).?\s*([ew])\s*(\d+)\D*(\d+\.\d+)/i;
+        m = re.exec(q.toLowerCase());
+        //showRegExpResult(m);
+        if ((m != undefined)) return m;
+        else return null;
+        // +- dec min +- dec min
+    },
 
 	_geocode : function (event) {
 		L.DomEvent.preventDefault(event);
+        var q = this._input.value;
+        //try to find corrdinates
+		if (this._isLatLon(q) != null)
+		{
+			var m = this._isLatLon(q);
+			console.log("LatLon: "+m[1]+" "+m[2]);
+			//m = {lon, lat}
+            this.options.callback.call(this, this._createSearchResult(m[1],m[2]));
+            return;
+		}
+		else if (this._isLatLon_decMin(q) != null)
+		{
+			var m = this._isLatLon_decMin(q);
+			//m: [ns, lat dec, lat min, ew, lon dec, lon min]
+			var temp  = new Array();
+			temp['n'] = 1;
+			temp['s'] = -1;
+			temp['e'] = 1;
+			temp['w'] = -1;
+            this.options.callback.call(this,this._createSearchResult(
+                temp[m[1]]*(Number(m[2]) + m[3]/60),
+			    temp[m[4]]*(Number(m[5]) + m[6]/60)
+            ));
+            return;
+		}
+
+        //and now Nominatim
 		//http://wiki.openstreetmap.org/wiki/Nominatim
 		this._callbackId = "_l_osmgeocoder_" + (this._callbackId++);
 		window[this._callbackId] = L.Util.bind(this.options.callback, this);
